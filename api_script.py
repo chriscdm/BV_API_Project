5e971870e205e344cdc0c3fb1289625f199cf980
Chris Marker <chris.d.marker@gmail.com>
2019-03-14 16:01:28 -0400

    cleaned up JSON output

--- api_script.py
+++ api_script.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, g
 #from flask_sqlalchemy import SQLAlchemy
 #from flask_marshmallow import Marshmallow
 import sqlite3
@@ -12,12 +12,18 @@ app.config["DEBUG"] = True
 def home():
     return "<h1>Home</h1>"
 
+
+def make_dicts(cursor, row):
+    return dict((cursor.description[idx][0], value)
+                for idx, value in enumerate(row))
+
 @app.route('/all_songs', methods=['GET'])
 def api_all():
     cnx = sqlite3.connect('/Users/christophermarker/Documents/BV_API_Project/bvde.db')
-    all_songs = cnx.execute('SELECT * FROM songs;').fetchall()
+    cnx.row_factory = make_dicts
+    cur = cnx.cursor()
+    all_songs = cur.execute('SELECT title,artist,name,duration FROM songs INNER JOIN genres ON songs.genre=genres.id;').fetchall()
 
     return jsonify(all_songs)
-
 
 app.run()
